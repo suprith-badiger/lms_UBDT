d04cdc88431f64a8aef3750b2cce7fa8e1612807
suprith-badiger <suprithbadiger02@gmail.com>
2024-05-06 11:07:42 +0530

    initial commit

---
+++ server/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      service: "gmail",
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+    console.log(info);
+    return info;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = mailSender;
